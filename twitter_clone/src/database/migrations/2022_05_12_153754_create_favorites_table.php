1e45b9a5a5ceebbfe73d57f66dec1062baaaf48c
nanbakeito <105186130+nanbakeito@users.noreply.github.com>
2022-05-14 12:17:25 +0900

    first commit

---
+++ twitter_clone/src/database/migrations/2022_05_12_153754_create_favorites_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained()->comment('ユーザID');
+            $table->foreignId('tweet_id')->constrained()->comment('ツイートID');
+    
+            $table->unique(['user_id', 'tweet_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
---
+++ twitter_clone/src/database/migrations/2022_05_12_153802_create_followers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->foreignId('following_id')->constrained('users')->comment('フォローしているユーザーのID');
+            $table->foreignId('followed_id')->constrained('users')->comment('フォローされているユーザーのID');
+    
+            $table->unique(['following_id', 'followed_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
