7ff344e1d4fe961cfe4f59bdb846efd799972bcb
nanbakeito <105186130+nanbakeito@users.noreply.github.com>
2022-06-13 22:22:34 +0900

    lang記述修正

--- twitter_clone/src/resources/views/auth/login.blade.php
+++ twitter_clone/src/resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                         @csrf
                         <!-- メール -->
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('メールアドレス') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"  required autocomplete="email">
                                 @error('email')
@@ -23,7 +23,7 @@
                         </div>
                         <!-- パスワード -->
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('パスワード') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 @error('password')
@@ -37,7 +37,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('ログイン') }}
+                                    ログイン
                                 </button>
                             </div>
                         </div>
